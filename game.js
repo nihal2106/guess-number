1533596b21a2a86e5f4503e87f6a8be343424e2a
Nihal <nihaljonnala@gmail.com>
2020-09-19 13:34:39 +0530

    modified images and names

--- game.js
+++ game.js
@@ -2,12 +2,13 @@ const $submitButton = document.getElementById("submit");
 const $message = document.getElementById("message");
 const $lives = document.getElementById("lives");
 
-var guessNumber = math.round(Math.random()*100);
+var guessNumber = Math.round(Math.random()*100);
 var lives = 10;
 var message;
 
 $submitButton.onclick = () => {
-    let userInput = document.getElementById("number-input").nodeValue;
+    let userInput = document.getElementById("number-input").value;
+    // here use .value not nodeValue. 
     lives--;
     if(userInput == guessNumber){
         location.href="./win.html";
@@ -16,12 +17,12 @@ $submitButton.onclick = () => {
         location.href="./lose.html";
     }
     else if(userInput > guessNumber){
-        message = 'Oops !  Your guess is to high. You have ${lives} lives reamining';
+        message = 'Oops !  Your guess is to high. ';
     }
     else if(userInput < guessNumber){
-        message = 'Oops !  Your guess is to low. You have ${lives} lives reamining';
+        message = 'Oops !  Your guess is to low. ';
     }
 $message.style.display = "inherit";
 $message.innerHTML = message;
 $lives.innerHTML = lives;
-};
+}
